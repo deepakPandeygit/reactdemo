66f93da6710269dbb7bc4ea576b0fdf088c1ea6c
Deepak Pandey <deepak.offsureit@yopmail.com>
2022-01-25 18:17:46 +0530

    done changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.scss';
 
 function App() {
+  const a=document.getElementById('qwe');
+  console.log('a',a)
   return (
-    <div className="name">
+    <div className="name" id="qwe">
       <h1>hello</h1>
     </div>
   );
 }
-
 export default App;
+
